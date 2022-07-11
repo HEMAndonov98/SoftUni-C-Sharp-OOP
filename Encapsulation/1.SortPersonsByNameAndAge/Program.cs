265a311027833c05d4ede4358d698590ea103769
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2022-07-11 18:23:20 +0300

    Lab

---
+++ Encapsulation/1.SortPersonsByNameAndAge/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var person = new Person("Carl", "Jung", 30, 2000);
+            person.IncreaseSalary(50);
+        }
+    }
+}
---
+++ Encapsulation/2.Salary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2.Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
